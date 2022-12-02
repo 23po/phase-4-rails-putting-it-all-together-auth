35fdfda4198e5d0369aacd6259e24141d22178ce
calvin <ckodalo@gmail.com>
2022-12-02 11:34:05 +0300

    passed sessions_spec.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,22 @@
 class SessionsController < ApplicationController
+    
     def create
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, status: :created
         else
-            render json: { error: "Invalid username or passsword" }, status: :unauthorized
+            render json: { errors: ["Invalid username or passsword"] }, status: :unauthorized
         end
 
     end
 
     def destroy
-        session.delete :user_id
-        head :no_content
+        if session[:user_id]
+            session.delete :user_id
+            head :no_content
+        else
+            render json: { errors: ["Invalid username or passsword"] }, status: :unauthorized
+        end
     end
 end
